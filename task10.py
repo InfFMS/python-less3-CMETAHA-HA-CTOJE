1fa28aab2a98604ec9f8138f847a82150855fcb8
yaroslav.alelekov <yaroslav.alelekov@gmail.com>
2024-10-21 14:18:41 +0700

    Последние три задания

--- task10.py
+++ task10.py
@@ -3,5 +3,25 @@
 # (которые делятся на сами не себя и на 1).
 # Если таких чисел не было, вывести "нет".
 
+def get_primes(n):
+    n += 1
+    sieve = [True] * n
+    primes = []
+    for p in range(2, n):
+        if sieve[p]:
+            primes.append(p)
+        for i in range(p * p, n, p):
+            sieve[i] = False
+    return primes[-1]
 
+
+num_prime_list = []
+for i in range(int(input('N >> '))):
+    num_in = int(input())
+    if num_in == get_primes(num_in):
+        num_prime_list.append(num_in)
+
+if len(num_prime_list) == 0: print('нет')
+elif min(num_prime_list) == max(num_prime_list): print(min(num_prime_list))
+else:print(min(num_prime_list), max(num_prime_list))
 
--- task8.py
+++ task8.py
@@ -1,3 +1,12 @@
 # с клавиатуры вводится число N, а затем – N целых чисел.
 # Определить, сколько было введено положительных и
 # сколько отрицательных чисел (нули не считать!).
+countPlus, countMinus = 0, 0
+for i in range(int(input('N >> '))):
+    num_in = int(input())
+    if num_in > 0: countPlus += 1
+    elif num_in < 0: countMinus += 1
+print(countPlus, countMinus)
+
+
+
